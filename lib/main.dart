2e16f6ecd6c5e86c5adb8da76c75204673a54c34
alefbispo <Blacksabbath21@>
2022-06-07 23:24:45 -0300

    formatatacao visual

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:food_recipes/data/dummy_data.dart';
+
+import 'screens/categories_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -14,29 +15,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({
-    Key? key,
-  }) : super(key: key);
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    List listaCategoria = DUMMY_CATEGORIES;
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Vamos Cozinhar?'),
-      ),
-      body: const Center(),
+      home: const CategoriesScreen(),
     );
   }
 }
--- lib/screens/categories_screen.dart
+++ lib/screens/categories_screen.dart
@@ -1,18 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:food_recipes/components/category_item.dart';
+import 'package:food_recipes/data/dummy_data.dart';
 
 class CategoriesScreen extends StatelessWidget {
   const CategoriesScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return GridView(
-      gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
-        maxCrossAxisExtent: 200,
-        childAspectRatio: 3 / 2,
-        crossAxisSpacing: 20,
-        mainAxisSpacing: 20,
+    return Scaffold(
+      appBar: AppBar(
+        title: const Center(
+          child: Text(
+            'Vamos Cozinhar',
+          ),
+        ),
       ),
-      children: const <Widget>[],
+      body: GridView(
+          gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+            maxCrossAxisExtent: 200,
+            childAspectRatio: 3 / 2,
+            crossAxisSpacing: 20,
+            mainAxisSpacing: 20,
+          ),
+          children: DUMMY_CATEGORIES.map((cat) {
+            return CategoryItem(cat);
+          }).toList()),
     );
   }
 }
