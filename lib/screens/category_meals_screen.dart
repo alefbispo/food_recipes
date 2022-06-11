52c3431d12bf7baf4fb7b482320860b26c09c59a
alefbispo <Blacksabbath21@>
2022-06-10 22:52:31 -0300

    utilizando metodo where para receber dados na pagina dos itens

--- lib/screens/category_meals_screen.dart
+++ lib/screens/category_meals_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:food_recipes/data/dummy_data.dart';
 
 import '../models/category.dart';
 
@@ -6,13 +7,22 @@ class CategoryMealsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final category = ModalRoute.of(context)!.settings.arguments as Category;
+
+    final categoryMeals = DUMMY_MEALS
+        .where(
+          (meal) => meal.categories.contains(category.id),
+        )
+        .toList();
+
     return Scaffold(
       appBar: AppBar(
         title: Text(category.title),
       ),
-      body: Center(
-        child: Text(category.title),
-      ),
+      body: ListView.builder(
+          itemCount: categoryMeals.length,
+          itemBuilder: ((context, index) {
+            return Text(categoryMeals[index].title);
+          })),
     );
   }
 }
